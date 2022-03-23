7ac63700ee84a2e961ef9bd0cf5bab3d6765e6d6
vlad-mi <93992056+vlad-mi@users.noreply.github.com>
2022-03-23 19:05:20 +0100

    Test (#1)

--- src/main/java/com/movingimage/test/TestClass.java
+++ src/main/java/com/movingimage/test/TestClass.java
@@ -2,6 +2,6 @@ package com.movingimage.test;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Test 1");
     }
 }
