424c5f8cac946e4facdc0397dfb452a0bba526be
Vladislav Gerasimov <vladislav.gerasimov@movingimage.com>
2022-03-24 09:42:44 +0100

    Test

--- src/main/java/com/movingimage/test/TestClass.java
+++ src/main/java/com/movingimage/test/TestClass.java
@@ -2,6 +2,6 @@ package com.movingimage.test;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("Test 5");
+        System.out.println("Test");
     }
 }
